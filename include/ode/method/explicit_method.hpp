eed2ffe48c99fd9f22da1e1b73fb6f13bdf21481
acdemiralp <demiralpali@gmail.com>
2021-10-26 19:15:55 +0200

    Refactored explicit method. Added more stubs.

--- include/ode/method/explicit_method.hpp
+++ include/ode/method/explicit_method.hpp
@@ -9,31 +9,31 @@
 
 namespace ode
 {
-template <typename butcher_tableau>
+template <typename tableau>
 class explicit_method
 {
-  using t_type    = typename butcher_tableau::type;
+  using t_type    = typename tableau::type;
   template <typename y_type>
   using dydt_type = std::function<y_type(const y_type&, t_type)>;
 
   template <typename y_type>
   static constexpr y_type evaluate(const y_type& y, t_type t, const dydt_type<y_type>& f, t_type h)
   {
-    std::array<y_type, butcher_tableau::stages> k;
-    constexpr_for<0, butcher_tableau::stages, 1>([&y, &t, &f, &h, &k] (auto i)
+    std::array<y_type, tableau::stages> k;
+    constexpr_for<0, tableau::stages, 1>([&y, &t, &f, &h, &k] (auto i)
     {
       y_type sum;
       constexpr_for<0, i.value, 1>([&k, &sum, &i] (auto j)
       {
-        sum += k[j] * std::get<triangular_number<i.value - 1> + j.value>(butcher_tableau::a);
+        sum += k[j] * std::get<triangular_number<i.value - 1> + j.value>(tableau::a);
       });
-      k[i] = f(y + sum * h, t + std::get<i>(butcher_tableau::c) * h);
+      k[i] = f(y + sum * h, t + std::get<i>(tableau::c) * h);
     });
 
     y_type sum;
-    constexpr_for<0, butcher_tableau::stages, 1>([&k, &sum] (auto i)
+    constexpr_for<0, tableau::stages, 1>([&k, &sum] (auto i)
     {
-      sum += k[i] * std::get<i>(butcher_tableau::b);
+      sum += k[i] * std::get<i>(tableau::b);
     });
     return y + sum * h;
   }
