c01b9ce4cb40724d8b0d3d4b4e3d54968be84696
acdemiralp <demiralpali@gmail.com>
2021-10-27 18:55:18 +0200

    Added explicit method test (note: Eigen is used in tests, but the library does not depend on it!).

--- tests/ode_test.cpp
+++ tests/ode_test.cpp
@@ -1,8 +1,21 @@
 #include <doctest/doctest.h>
 
-#include <ode/ode.hpp>
+#include <Eigen/Core>
+
+#include <ode/method/explicit_method.hpp>
+#include <ode/tableau/explicit/forward_euler.hpp>
 
 TEST_CASE("ODE Test")
 {
-  
+  using method = ode::explicit_method<ode::forward_euler_tableau<float>>;
+
+  const Eigen::Vector3f y0 (0.0f, 0.0f, 0.0f);
+  const float           t0 (0.0f);
+  const float           h  (1.0f);
+  const std::function   f  = [ ] (const Eigen::Vector3f& y, const float h)
+  {
+    return Eigen::Vector3f(1.0f, 0.0f, 0.0f);
+  };
+
+  auto result = method::evaluate(y0, t0, f, h);
 }
