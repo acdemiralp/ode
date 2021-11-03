b93e072e5dd9462e493ed5b4abf4f9ac0dbd2631
ACD <demiralpali@gmail.com>
2021-11-03 04:09:17 +0100

    Added code to decompose any order IVP to a set of coupled first order IVPs.

--- include/ode/problem/initial_value_problem.hpp
+++ include/ode/problem/initial_value_problem.hpp
@@ -13,45 +13,42 @@ struct initial_value_problem
   using value_type    = value_type_;
   using function_type = std::function<value_type(time_type, const value_type&)>;
 
-  time_type     time      ;
-  value_type    value     ;
-  function_type function  ;
-};
-
-template<typename type, std::size_t order, typename = std::make_index_sequence<order>>
-struct expander;
-template<typename type, std::size_t order, std::size_t... sequence>
-struct expander<type, order, std::index_sequence<sequence...>>
-{
-  template<std::size_t>
-  using type = type;
-
-  std::function<std::size_t(decltype(sequence)...)> foo;
+  time_type     time    ;
+  value_type    value   ;
+  function_type function;
 };
 
 // Higher order initial value problems are decomposed into coupled first order initial value problems.
 template <typename time_type_, typename value_type_, std::size_t order, typename = std::make_index_sequence<order>>
 struct initial_value_problem_n;
-
 template <typename time_type_, typename value_type_, std::size_t order, std::size_t... sequence>
 struct initial_value_problem_n<time_type_, value_type_, order, std::index_sequence<sequence...>>
 {
-  using time_type     = time_type_ ;
-  using value_type    = value_type_;
-  using function_type = std::function<value_type(time_type, const std::array<value_type, order>&)>;
+  using time_type           = time_type_ ;
+  using value_type          = value_type_;
+  template <std::size_t>
+  using expanded_value_type = value_type;
+  using function_type       = std::function<value_type(time_type, const expanded_value_type<sequence>&...)>;
 
-  std::unique_ptr<std::array<initial_value_problem<time_type, value_type>, order>> to_coupled_initial_value_problems()
+  template <std::size_t variable_index, std::size_t sequence_index>
+  const value_type& selector(const value_type& value, const std::array<initial_value_problem<time_type, value_type>, order>& result) const
+  {
+    if constexpr (variable_index == sequence_index)
+      return value;
+    else
+      return std::get<sequence_index>(result).value;
+  }
+
+  std::unique_ptr<std::array<initial_value_problem<time_type, value_type>, order>> to_coupled_initial_value_problems() const
   {
     auto result = std::make_unique<std::array<initial_value_problem<time_type, value_type>, order>>();
-    constexpr_for<0, order, 1>([&] (auto& i)
+    constexpr_for<0, order, 1>([&] (auto i)
     {
       std::get<i>(*result).time     = time;
       std::get<i>(*result).value    = std::get<i>(values);
       std::get<i>(*result).function = [=, &result] (time_type time, const value_type& value)
       {
-        return function(time, {value          , result[1].value, result[2].value});
-        return function(time, {result[0].value, value          , result[2].value}); // TODO
-        return function(time, {result[0].value, result[1].value, value          }); // TODO
+        return function(time, selector<i.value, sequence>(value, *result)...);
       };
     });
     return std::move(result);
--- tests/ode_test.cpp
+++ tests/ode_test.cpp
@@ -41,4 +41,18 @@ TEST_CASE("ODE Test")
   REQUIRE(iterator->value[0] == 2.0f);
   REQUIRE(iterator->value[1] == 0.0f);
   REQUIRE(iterator->value[2] == 0.0f);
+
+  using second_order_problem_type = ode::initial_value_problem_n<float, Eigen::Vector3f, 2>;
+
+  const auto second_order_problem = second_order_problem_type
+  {
+    0.0f,
+    std::array {Eigen::Vector3f(0.0f, 0.0f, 0.0f), Eigen::Vector3f(0.0f, 0.0f, 0.0f)},
+    [ ] (const float h, const Eigen::Vector3f& y, const Eigen::Vector3f& y2)
+    {
+      return y;
+    }
+  };
+
+  auto coupled_first_order_problems = second_order_problem.to_coupled_initial_value_problems();
 }
