1daea1c7b3ec6d2c04fcf77e73c8685c8d783d93
ACD <demiralpali@gmail.com>
2021-11-25 17:54:18 +0100

    Added adaptive integration test.

--- tests/adaptive_test.cpp
+++ tests/adaptive_test.cpp
@@ -7,14 +7,16 @@
 
 TEST_CASE("Adaptive Test")
 {
-  using method_type    = ode::explicit_method<ode::dormand_prince_5_tableau<float>>;
-  using problem_type   = ode::initial_value_problem<float, Eigen::Vector3f>;
+  using method_type             = ode::explicit_method<ode::dormand_prince_5_tableau<float>>;
+  using problem_type            = ode::initial_value_problem<float, Eigen::Vector3f>;
+  using i_controller_iterator   = ode::adaptive_step_iterator<method_type, problem_type, ode::integral_controller                        <method_type, problem_type>>;
+  using pi_controller_iterator  = ode::adaptive_step_iterator<method_type, problem_type, ode::proportional_integral_controller           <method_type, problem_type>>;
+  using pid_controller_iterator = ode::adaptive_step_iterator<method_type, problem_type, ode::proportional_integral_derivative_controller<method_type, problem_type>>;
 
-  constexpr auto sigma = 10.0f;
-  constexpr auto rho   = 28.0f;
-  constexpr auto beta  = 8.0f / 3.0f;
-
-  const auto problem   = problem_type
+  constexpr auto sigma   = 10.0f;
+  constexpr auto rho     = 28.0f;
+  constexpr auto beta    = 8.0f / 3.0f;
+  const     auto problem = problem_type
   {
     0.0f,                                         /* t0 */
     Eigen::Vector3f(16.0f, 16.0f, 16.0f),         /* y0 */
@@ -24,7 +26,15 @@ TEST_CASE("Adaptive Test")
     }
   };
 
-  auto iterator = ode::adaptive_step_iterator<method_type, problem_type, ode::integral_controller<method_type, problem_type>>(problem, 1.0f /* h */);
-  for (auto i = 0; i < 1000; ++i)
-    ++iterator;
+  auto iterator_1 = i_controller_iterator  (problem, 1.0f /* h */);
+  for (auto i = 0; i < 10000; ++i)
+    ++iterator_1;
+
+  auto iterator_2 = pi_controller_iterator (problem, 1.0f /* h */);
+  for (auto i = 0; i < 10000; ++i)
+    ++iterator_2;
+
+  auto iterator_3 = pid_controller_iterator(problem, 1.0f /* h */);
+  for (auto i = 0; i < 10000; ++i)
+    ++iterator_3;
 }
