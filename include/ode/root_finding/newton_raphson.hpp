81f52f3fa6489612124324a2a3858d8e826dbc96
acdemiralp <demiralpali@gmail.com>
2022-08-11 01:56:43 +0200

    Eigen is no longer only for tests as it is necessary for e.g. LU factorization in Newton-Raphson. Optionally enabled for (semi-)implicit method support.

--- include/ode/root_finding/newton_raphson.hpp
+++ include/ode/root_finding/newton_raphson.hpp
@@ -2,5 +2,17 @@
 
 namespace ode
 {
-
+template <
+  typename time_type               ,
+  typename value_type              , 
+  typename function_type           , 
+  typename derivative_function_type>
+constexpr value_type newton_raphson(
+  const time_type                 time               ,
+  const value_type&               value              , 
+  const function_type&            function           , 
+  const derivative_function_type& derivative_function)
+{
+  return value - function(time, value) / derivative_function(time, value);
+}
 }
